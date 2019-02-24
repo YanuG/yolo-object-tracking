bbc42763e102466e43c5ae60629b2c057e380af3
Yanushka Gnanamuttu <ygnan057@uottawa.ca>
2019-02-23 21:32:31 -0500

    Update and rename image_convertor.py to image_converter.py

--- src/image_converter.py
+++ src/image_converter.py
@@ -5,14 +5,24 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class image_convertor: 
+class ImageConverter: 
 
     def __init__(self):
         self.image_pub = rospy.Publisher("Jetson_Camera",Image)
         self.bridge = CvBridge()
-
+    
+    
     def read_cam(self):
-        cap = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1 ! nvtee ! nvvidconv flip-method=2 ! video/x-raw(memory:NVMM), format=(string)I420 ! nvoverlaysink -e ! appsink")
+        # open Jetson Camera
+        cap = cv2.VideoCapture(
+            "nvcamerasrc ! video/x-raw(memory:NVMM), 
+            width=(int)1920, 
+            height=(int)1080, 
+            format=(string)I420, 
+            framerate=(fraction)30/1 ! nvtee ! nvvidconv flip-method=2 ! video/x-raw(memory:NVMM), 
+            format=(string)I420 ! nvoverlaysink -e ! appsink"
+        )
+        # read image from camera
         if cap.isOpened():
             while True:
                 _, img = cap.read()
@@ -20,15 +30,14 @@ class image_convertor:
                 try:
                     self.image_pub.publish(self.bridge.cv2_to_imgmsg(img, "bgr8"))
                 except CvBridgeError as e:
-                    print(e)
-    
+                    print(e)    
 
+                    
 if __name__ == '__main__':
-    ic = image_convertor()
+    ic = ImageConverter()
     rospy.init_node('image_converter', anonymous=True)
     try:
         ic.read_cam()
-        rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
         cv2.destroyAllWindows()
