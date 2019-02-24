077b062b87bbb92b1debd9359b246c550f7be67a
Yanushka <ygnana057@uottawa.ca>
2019-02-24 12:25:03 -0500

    Fixed parameters to open camera

--- src/image_converter.py
+++ src/image_converter.py
@@ -8,29 +8,34 @@ from cv_bridge import CvBridge, CvBridgeError
 class ImageConverter: 
 
     def __init__(self):
-        self.image_pub = rospy.Publisher("Jetson_Camera",Image)
+        self.image_pub = rospy.Publisher("Jetson_Camera",Image, queue_size=1)
         self.bridge = CvBridge()
     
     
     def read_cam(self):
+        width = 416
+        height = 416
         # open Jetson Camera
-        cap = cv2.VideoCapture(
-            "nvcamerasrc ! video/x-raw(memory:NVMM), 
-            width=(int)1920, 
-            height=(int)1080, 
-            format=(string)I420, 
-            framerate=(fraction)30/1 ! nvtee ! nvvidconv flip-method=2 ! video/x-raw(memory:NVMM), 
-            format=(string)I420 ! nvoverlaysink -e ! appsink"
-        )
+        gst_str = ('nvcamerasrc ! '
+               'video/x-raw(memory:NVMM), '
+               'width=(int)2592, height=(int)1458, '
+               'format=(string)I420, framerate=(fraction)30/1 ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! '
+               'videoconvert ! appsink').format(width, height)
+        cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
         # read image from camera
         if cap.isOpened():
             while True:
                 _, img = cap.read()
-                cv2.imshow("Image window", img)
+                cv2.imshow("Jetson TX2 Camera", img)
                 try:
                     self.image_pub.publish(self.bridge.cv2_to_imgmsg(img, "bgr8"))
                 except CvBridgeError as e:
-                    print(e)    
+                    print(e)  
+        else:
+            print "Unable to open Cap"
 
                     
 if __name__ == '__main__':
