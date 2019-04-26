a740247014a75d1914200cda9bde7be8e3c1b557
nvidia <nvidia@nvidia.com>
2019-04-26 13:52:09 -0400

    Update drawer

--- src/drawer/drawer.py
+++ src/drawer/drawer.py
@@ -10,20 +10,15 @@ class Draw:
     def addBox(self, xmin, ymin, xmax, ymax, id):
         x = xmin
         y = ymin 
-        w = xmax - ymax
+        w = xmax - xmin
         h = ymax - ymin
         color = (randint(0, 255), randint(0, 255), randint(0, 255))
-        cv2.rectangle(self.image, (x, y), (x + w, y + h), color)
+        cv2.rectangle(self.image, (x, y), (x + w, y + h), color, 2)
         text_size = cv2.getTextSize(id, cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 1)
-        print
-        cv2.rectangle(self.image, (x + w, y + h) , (text_size.width + 3, text_size.height + 4), color, -1)
-        cv2.putText(self.image, id, (x, y + text_size.height),
-                     cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255, 255, 255), 1, cv2.CV_AA)
+        cv2.putText(self.image, id, (x, y - 3),
+                     cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255, 255, 255), 1)
     
     def displayImage(self):
         cv2.imshow(self.output_name, self.image)
-        cv2.waitKey(1)
-
-
-
+        cv2.waitKey(0)
  
